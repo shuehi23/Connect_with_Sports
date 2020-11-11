6341cd61dbb2b1c058079bc8297208cfd2b845e1
shuehi23 <23.shuhei.18@gmail.com>
2020-11-11 18:29:12 +0900

    本番環境

--- function.php
+++ function.php
@@ -86,16 +86,18 @@ $gestUserEmail = 'guest@login.com';
 //DB接続関数
 function dbConnect()
 {
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    
-    $db_name = substr($url["path"], 1);
-    $db_host = $url["host"];
-    $user = $url["user"];
-    $password = $url["pass"];
-      
-    $dsn = "mysql:dbname=".$db_name.";host=".$db_host;
-
-    $pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+    $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+    $db['dbname'] = ltrim($db['path'], '/');
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $user = $db['user'];
+    $password = $db['pass'];
+    $options = array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+    );
+    $dbh = new PDO($dsn,$user,$password,$options);
+    return $dbh;
     // $dsn = 'mysql:dbname=one_sports;host=localhost;charset=utf8';
     // $user = 'root';
     // $pass = 'root';
@@ -109,7 +111,7 @@ function dbConnect()
     //     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
     // );
     // $dbh = new PDO($dsn, $user, $pass, $option);
-    return $pdo;
+    // return $dbh;
 }
 // SQL実行関数
 function queryPost($dbh, $sql, $data)
